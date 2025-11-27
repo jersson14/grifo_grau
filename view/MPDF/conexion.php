a6ffa497f75469b305da619505e19f5b4a699779
Jersson <jersson1407miranda@gmail.com>
2025-11-27 16:48:47 -0500

    se modifico conexion

--- view/MPDF/conexion.php
+++ view/MPDF/conexion.php
@@ -1,4 +1,6 @@
 <?php
+date_default_timezone_set('America/Lima');
+
 $mysqli = new mysqli("localhost", "root", "", "grifo_grau2", 3307);
 
 // Verificar conexión
@@ -6,4 +8,7 @@ if ($mysqli->connect_errno) {
     echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     exit();
 }
+
+// Configurar zona horaria en MySQL
+$mysqli->query("SET time_zone = '-05:00'");
 ?>
